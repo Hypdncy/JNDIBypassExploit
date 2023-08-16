c2dda66620c5af18a56a38dfd9ae03437a441567
Hypdncy <Hypdncy@outlook.com>
2023-08-16 11:29:37 +0800

    git init

---
+++ src/main/java/com/feihong/ldap/enumtypes/PayloadType.java
@@ -0,0 +1,20 @@
+package com.feihong.ldap.enumtypes;
+
+public enum PayloadType {
+    command,
+    dnslog,
+    reverseshell,
+    tomcatecho,
+    springecho,
+    weblogicecho,
+    tomcatmemshell1,
+    tomcatmemshell2,
+    tomcatmemshell3,
+    weblogicmemshell1,
+    weblogicmemshell2,
+    jettymemshell,
+    jbossmemshell,
+    webspherememshell,
+    springmemshell;
+
+}
